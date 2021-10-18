d38f090eb3cd3300e3966b1545b88d2110ea2e4d
Omar <otorresd1@gmail.com>
2021-10-18 18:05:41 -0400

    ...

--- app/src/main/java/com/otorresd/ram/MainActivity.kt
+++ app/src/main/java/com/otorresd/ram/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
